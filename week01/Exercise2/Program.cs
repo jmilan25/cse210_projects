bafc0ebee8869c1c0146e7241f063eaad07b083f
jmilan25 <jmilan@byupathway.edu>
2025-10-31 22:25:12 +0800

    finished assignments

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -5,5 +5,41 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise2 Project.");
+
+        Console.Write("grade: ");
+        string input = Console.ReadLine();
+        
+        int x = int.Parse(input);
+
+        if (x >= 90)
+        {
+            Console.WriteLine("You got an A");
+        }
+        else if (x >= 80)
+        {
+            Console.WriteLine("You got a B");
+        }
+        else if (x >= 70)
+        {
+            Console.WriteLine("You got a C");
+        }
+        else if (x >= 60)
+        {
+            Console.WriteLine("You got a D");
+        }
+        else
+        {
+            Console.WriteLine("You got an F");
+        }
+
+        if (x >= 70)
+        {
+            Console.Write("YOU PASSED!");
+        }
+
+        else
+        {
+            Console.Write("you failed... I'm sorry");
+        }
     }
 }
--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -5,5 +5,32 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise3 Project.");
+
+        Random randomGenerator = new Random();
+        int x = randomGenerator.Next(1, 100);
+
+        int y = -1;
+        
+        while (x != y)
+        {
+            Console.Write("What is your guess? ");
+            string guess = Console.ReadLine();
+            y = int.Parse(guess);
+
+            if (y > x)
+            {
+                Console.WriteLine("lower");
+            }
+            else if (y < x)
+            {
+                Console.WriteLine("Higher");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        }
+            
+
     }
 }
