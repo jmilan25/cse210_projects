b4caae6dbb0c953fa15b6a144461832a7ce708d9
jmilan25 <jmilan@byupathway.edu>
2025-11-01 20:58:16 +0800

    Assignment added

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -5,5 +5,60 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise5 Project.");
+
+        Console.WriteLine("Welcome to the program!");
+        string name = PromptUserName();
+        int number = PromptUserNumber();
+        int snumber = SquareNumber(number);
+        DisplayResult(name, snumber);
+
+        static string PromptUserName()
+        {
+            Console.Write("Please enter your name: ");
+            string name = Console.ReadLine();
+            return name;
+        }
+
+        static int PromptUserNumber()
+        {
+            Console.Write("Please enter your favorite number: ");
+            string number = Console.ReadLine();
+            int y = int.Parse(number);
+            return y;
+        }
+
+        static int SquareNumber(int number)
+        {
+            int sum = number * number;
+            return sum;
+        }
+
+        static void DisplayResult(string name, int sum)
+        {
+
+            Console.WriteLine($"{name}, the square of your number is {sum}");
+        }
+
+
+
+
+
+
+
+
+
+
+
+        // static void DisplayResult(string PromptUserName, int PromptUserNumber, int SquareNumber)
+        // static void DisplayResult()
+        // {
+
+
+
+
+        // }
+
+        // DisplayResult();
+
     }
 }
