a013addcad8dfb7e0c661c4fa294e4f33c4aea84
jmilan25 <jmilan@byupathway.edu>
2025-11-01 19:30:19 +0800

    updated assignment

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -13,6 +13,7 @@ class Program
 
         int y = -1;
         int sum = 0;
+        int gnum = 0;
 
         while (y != 0)
         {
@@ -20,7 +21,12 @@ class Program
             Console.Write("Enter a number (0 to quit): ");   
             string x = Console.ReadLine();
             y = int.Parse(x);
-            numbers.Add(y);
+            // numbers.Add(y);
+
+            if (y != 0)
+            {
+                numbers.Add(y);
+            }
         }
 
         foreach (int number in numbers)
@@ -30,9 +36,26 @@ class Program
             
             //Console.WriteLine($" test {number}");
 
-
         }
+
+        foreach (int number in numbers)
+        {
+            if (number > gnum)
+            {
+                gnum = number;
+            }
+        }
+
+
+
+        float average = ((float)sum) / numbers.Count;
+
+
+        
         Console.WriteLine($"The sum is: {sum}");
+        Console.WriteLine($"The average is: {average}");
+        Console.WriteLine($"The largest number is: {gnum}");
+        // Console.WriteLine(numbers.Count);
 
 
     }
